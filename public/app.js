c61cd323fc0c909875947f6bc4a90a2870981516
Ari Rios <aririos0719@gmail.com>
2025-05-07 01:05:01 -0400

    implicit grant -> code w/ pkce; client id changed; xhr -> fetch

--- public/app.js
+++ public/app.js
@@ -4,14 +4,11 @@
 
 // API
 
-let clientID = "86f84ad124234ce498d1afc7470f2428"; // please create your own Spotify ID when using this project (https://developer.spotify.com/dashboard)
-let redirectURL = "https://lukasticky.gitlab.io/spotify-to-m3u"
-let token = "";
+let clientID = "16dcb564d8274f44a41c86b4093c5d02"; // please create your own Spotify ID when using this project (https://developer.spotify.com/dashboard)
+let redirectURL = "https://aririos.com/spot-m3u";
 
-if (location.hostname == "127.0.0.1") {
-    redirectURL = "http://127.0.0.1:5500/public/";
-} else if (location.hostname == "localhost") {
-    redirectURL = "http://localhost:5500/public/";
+if (location.hostname == "localhost" || location.hostname == "127.0.0.1") {
+    redirectURL = "http://127.0.0.1:8000/public/";
 }
 
 // elements
@@ -29,13 +26,16 @@ let downloadBtn = document.getElementById("download-btn");
 function clog(message, level = 0) {
     switch (level) {
         case 1:
-            console.log("%c" + message, "color: hsl(240, 69%, 49%)");
+            console.log(message);
+            // console.log("%c" + message, "color: hsl(240, 69%, 49%)");
             break;
         case 2:
-            console.log("%c" + message, "color: hsl(147, 69%, 49%)");
+            console.warn(message);
+            // console.log("%c" + message, "color: hsl(147, 69%, 49%)");
             break;
         case 3:
-            console.log("%c" + message, "color: hsl(0, 100%, 50%)");
+            console.error(message);
+            // console.log("%c" + message, "color: hsl(0, 100%, 50%)");
             break;
         default:
             console.log(message);
@@ -47,56 +47,112 @@ function clog(message, level = 0) {
  * request access token
  */
 
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  
+const codeVerifier = generateRandomString(64);
+const sha256 = async(plain) => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest('SHA-256', data);
+}
+
+const base64encode = (input) => {
+return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
 // login btn behavior
-loginBtn.addEventListener("click", function() {
-    clog("Logging in...", 1)
-    let scopes = "playlist-read-private"
+loginBtn.addEventListener("click", async function() {
+    clog("Logging in...", 1);
+    let scopes = "playlist-read-private";
+
+    const hashed = await sha256(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+
+    // generated in the previous step
+    window.localStorage.setItem('code_verifier', codeVerifier);
 
     // request access token
-    window.location = `https://accounts.spotify.com/authorize?client_id=${ encodeURIComponent(clientID) }&response_type=token${
-        scopes ? "&scope=" + encodeURIComponent(scopes) : ""
-    }&redirect_uri=${ encodeURIComponent(redirectURL) }`;
+    window.location = 
+        'https://accounts.spotify.com/authorize?client_id=' + encodeURIComponent(clientID) + 
+        '&response_type=code' +
+        (scopes ? "&scope=" + encodeURIComponent(scopes) : "")+
+        '&redirect_uri=' + encodeURIComponent(redirectURL) +
+        '&code_challenge_method=S256' + 
+        '&code_challenge=' + encodeURIComponent(codeChallenge);
 });
 
 /*
  * login
  */
 
-// get token from URL
+// get code from URL
+let code = "";
 try {
-    token = window.location.hash.substr(1).split("=")[1].split("&")[0];
+    const urlParams = new URLSearchParams(window.location.search);
+    code = urlParams.get('code');
 } catch (error) {
-    token = ""
+    code = ""
 }
 
 // login
-if (token) {
-    
-    clog("Retrieving username...", 1)
+if (code) {
+    const getToken = async code => {
 
-    let XHR = new XMLHttpRequest();
-    XHR.open("GET", `https://api.spotify.com/v1/me?client_id=${ encodeURIComponent(clientID) }&access_token=${ encodeURIComponent(token) }`);
-    XHR.onreadystatechange = function() {
-        if (XHR.readyState === XMLHttpRequest.DONE) {
-            if (XHR.status === 200) {
-                
-                // get username
-                let username = JSON.parse(XHR.response).display_name;
-                clog("Logged in as: " + username, 2);
-                loginBtn.textContent = `Logged in as ${username}`;
-
-                // fetch playlists
-                fetchPlaylists();
-
-            } else {
-                alert("Something went wrong. Please login again")
-                window.location = redirectURL;
-            }
+        // stored in the previous step
+        const codeVerifier = localStorage.getItem('code_verifier');
+      
+        const url = "https://accounts.spotify.com/api/token";
+        const payload = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            client_id: clientID,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectURL,
+            code_verifier: codeVerifier,
+          }),
         }
-    };
-    XHR.send();
+      
+        const body = await fetch(url, payload);
+        const response = await body.json();
+      
+        localStorage.setItem('access_token', response.access_token);
+        console.log('Logged in');
+      };
+    if (!localStorage.getItem('access_token')) {
+        await getToken(code);
+    }
+}
 
-} else {
+// already logged in?
+
+async function getEndpoint(endpoint) {
+    let accessToken = localStorage.getItem('access_token');
+    
+    const response = await fetch(endpoint, {
+        headers: {
+        Authorization: 'Bearer ' + accessToken
+        }
+    });
+    
+    const data = await response.json();
+    return data;
+}
+if (localStorage.getItem('access_token')) {
+    let { id } = await getEndpoint('https://api.spotify.com/v1/me');
+    console.log('Logged in as ' + id);
+}
+else {
     loginBtn.disabled = false;
 }
 
@@ -104,7 +160,7 @@ if (token) {
  * fetch playlists
  */
 
-function fetchPlaylists() {
+async function fetchPlaylists() {
     
     clog("Retrieving playlists...", 1)
 
@@ -120,76 +176,63 @@ function fetchPlaylists() {
     let stepLimit = 50;
 
     // recursive function to fetch playlists
-    function fetchPaylistsRecursive(playlistOffset, playlistTotal) {
+    async function fetchPaylistsRecursive(playlistOffset, playlistTotal) {
+        let response = await getEndpoint(
+            'https://api.spotify.com/v1/me/playlists?client_id=' + encodeURIComponent(clientID) +
+            '&offset=' + encodeURIComponent(playlistOffset) +
+            '&limit=' + encodeURIComponent(stepLimit)
+        ); 
+        clog(`Retrieved tracks ${ playlistOffset }-${ playlistOffset + stepLimit }`, 2);
+        
+        // populate table
+        for (const playlist of response.items) {
+            
+            let tRow = tBody.insertRow();
+            
+            // name
+            tRow.insertCell().textContent = playlist.name;
+            
+            // tracks
+            tRow.insertCell().textContent = playlist.tracks.total;
 
-        let XHR = new XMLHttpRequest();
-        XHR.open("GET", `https://api.spotify.com/v1/me/playlists?client_id=${ encodeURIComponent(clientID) }&access_token=${ encodeURIComponent(token) }&offset=${ encodeURIComponent(playlistOffset) }&limit=${ encodeURIComponent(stepLimit) }`);
-        XHR.onreadystatechange = function() {
-            if (XHR.readyState === XMLHttpRequest.DONE) {
-                if (XHR.status === 200) {
-                    
-                    // get playlists
-                    let response = JSON.parse(XHR.response);
-                    playlistTotal = response.total;
-                    clog(`Retrieved tracks ${ playlistOffset }-${ playlistOffset + stepLimit }`, 2);
-                    
-                    // populate table
-                    for (const playlist of response.items) {
-                        
-                        let tRow = tBody.insertRow();
-                        
-                        // name
-                        tRow.insertCell().textContent = playlist.name;
-                        
-                        // tracks
-                        tRow.insertCell().textContent = playlist.tracks.total;
-    
-                        // create convert buttons
-                        let btn1 = document.createElement("button");
-                        btn1.innerHTML = "<code>.M3U</code>";
-                        btn1.addEventListener("click", function() {
-                            convertPlaylist(playlist.id, playlist.name, playlist.tracks.total, "m3u");
-                        });
-                        let btn2 = document.createElement("button");
-                        btn2.innerHTML = "<code>.M3U8</code>";
-                        btn2.addEventListener("click", function() {
-                            convertPlaylist(playlist.id, playlist.name, playlist.tracks.total, "m3u8");
-                        });
-    
-                        // append convert btns
-                        let tCell = tRow.insertCell();
-                        tCell.appendChild(btn1);
-                        tCell.appendChild(btn2);
-                    }
-    
-                    // append table
-                    playlistsSecCont.children[0].remove();
-                    playlistsSecCont.appendChild(table);
+            // create convert buttons
+            let btn1 = document.createElement("button");
+            btn1.innerHTML = "<code>.M3U</code>";
+            btn1.addEventListener("click", function() {
+                convertPlaylist(playlist.id, playlist.name, playlist.tracks.total, "m3u");
+            });
+            let btn2 = document.createElement("button");
+            btn2.innerHTML = "<code>.M3U8</code>";
+            btn2.addEventListener("click", function() {
+                convertPlaylist(playlist.id, playlist.name, playlist.tracks.total, "m3u8");
+            });
 
-                    if (playlistOffset + stepLimit < playlistTotal) {
-                        // call `fetchPaylistsRecursive(playlistOffset + stepLimit, playlistTotal)` again for the next batch
-                        fetchPaylistsRecursive(playlistOffset + stepLimit, playlistTotal)
-                    }
+            // append convert btns
+            let tCell = tRow.insertCell();
+            tCell.appendChild(btn1);
+            tCell.appendChild(btn2);
+        }
+
+        // append table
+        playlistsSecCont.children[0].remove();
+        playlistsSecCont.appendChild(table);
+
+        if (playlistOffset + stepLimit < playlistTotal) {
+            // call `fetchPaylistsRecursive(playlistOffset + stepLimit, playlistTotal)` again for the next batch
+            await fetchPaylistsRecursive(playlistOffset + stepLimit, playlistTotal)
+        }
     
     
-                } else {
-                    alert("Something went wrong. Please login again")
-                    window.location = redirectURL;
-                }
-            }
-        };
-        XHR.send();
     }
-
     // call `fetchPaylistsRecursive(tracksOffset, tracksTotal)` for the first time
-    fetchPaylistsRecursive(0, 0)
+    await fetchPaylistsRecursive(0, 0);
 }
 
 /*
  * convert playlist to M3U(8)
  */
 
-function convertPlaylist(id, playlistName, tracksTotal, type) {
+async function convertPlaylist(id, playlistName, tracksTotal, type) {
 
     clog("Retrieving tracks...", 1)
     
@@ -203,82 +246,76 @@ function convertPlaylist(id, playlistName, tracksTotal, type) {
     let stepLimit = 100;
 
     // recursive function to fetch tracks
-    function fetchTracksRecursive(tracksOffset, tracksTotal) {
-        let XHR = new XMLHttpRequest();
-        XHR.open("GET", `https://api.spotify.com/v1/playlists/${ encodeURIComponent(id) }/tracks?client_id=${ encodeURIComponent(clientID) }&access_token=${ encodeURIComponent(token) }&offset=${ encodeURIComponent(tracksOffset) }&limit=${ encodeURIComponent(stepLimit) }`);
-        XHR.onreadystatechange = function() {
-            if (XHR.readyState === XMLHttpRequest.DONE) {
-                if (XHR.status === 200) {
+    async function fetchTracksRecursive(tracksOffset, tracksTotal) {
+        let playlist = await getEndpoint(
+            'https://api.spotify.com/v1/playlists/' + encodeURIComponent(id) + '/tracks' +
+            '?client_id='+ encodeURIComponent(clientID) +
+            '&offset=' + encodeURIComponent(tracksOffset) +
+            '&limit=' + encodeURIComponent(stepLimit)
+        );
+
                     
-                    // get tracks
-                    let playlist = JSON.parse(XHR.response);
-                    clog(`Retrieved tracks ${ tracksOffset }-${ tracksOffset + stepLimit }`, 2);
-    
-                    // populate M3U file
-                    for (const item of playlist.items) {
-                        
-                        if (type == "m3u") {
-    
-                            // 1st line
-                            fileContent += `#EXTINF:${Math.floor(item.track.duration_ms / 1000)},${ (function() {
-                                let artists = [];
-                                for (artist of item.track.artists) {
-                                    artists.push(encodeURIComponent(artist.name));
-                                }
-                                return artists.join("; ");
-                            })()} - ${item.track.name}\n`;
-                            
-                            // 2nd line
-                            fileContent += `${ encodeURIComponent(item.track.artists[0].name) }%20-%20${ encodeURIComponent(item.track.name) }.mp3\n`;
-    
-                        } else if (type == "m3u8") {
-    
-                            // 1st line
-                            fileContent += `#EXTINF:${Math.floor(item.track.duration_ms / 1000)},${ (function() {
-                                let artists = [];
-                                for (artist of item.track.artists) {
-                                    artists.push(artist.name);
-                                }
-                                return artists.join("; ");
-                            })()} - ${item.track.name}\n`;
-        
-                            // 2nd line
-                            fileContent += `${ item.track.artists[0].name } - ${ item.track.name }.mp3\n`;
-                        }
-    
-                        progress.value++;
+        clog(`Retrieved tracks ${ tracksOffset }-${ tracksOffset + stepLimit }`, 2);
+
+        // populate M3U file
+        for (const item of playlist.items) {
+            
+            if (type == "m3u") {
+
+                // 1st line
+                fileContent += `#EXTINF:${Math.floor(item.track.duration_ms / 1000)},${ (function() {
+                    let artists = [];
+                    for (let artist of item.track.artists) {
+                        artists.push(encodeURIComponent(artist.name));
                     }
+                    return artists.join("; ");
+                })()} - ${item.track.name}\n`;
+                
+                // 2nd line
+                fileContent += `${ encodeURIComponent(item.track.artists[0].name) }%20-%20${ encodeURIComponent(item.track.name) }.mp3\n`;
 
-                    clog(tracksOffset);
-                    if (tracksOffset >= tracksTotal) {
+            } else if (type == "m3u8") {
 
-                        // create file
-                        let file = new Blob([fileContent], {type: type});
-
-                        // prepare download
-                        clog("Preparing download...", 1);
-                        downloadBtn.disabled = false;
-                        downloadA.download = `${ playlistName }.${ type }`;
-                        downloadA.href = URL.createObjectURL(file);
-                        downloadBtn.innerHTML = `Download <code>${ playlistName }.${ type.toUpperCase() }</code>`
-                        
-                        // initiate download
-                        downloadA.click();
-                        clog(`Downloaded file \`${ playlistName }.${ type }\``, 2);
-                    } else {
-                        // call `fetchTracksRecursive(tracksOffset, tracksTotal)` again for the next batch
-                        fetchTracksRecursive(tracksOffset + stepLimit, tracksTotal)
+                // 1st line
+                fileContent += `#EXTINF:${Math.floor(item.track.duration_ms / 1000)},${ (function() {
+                    let artists = [];
+                    for (let artist of item.track.artists) {
+                        artists.push(artist.name);
                     }
-    
-                } else {
-                    alert("Something went wrong. Please login again")
-                    window.location = redirectURL;
-                }
+                    return artists.join("; ");
+                })()} - ${item.track.name}\n`;
+
+                // 2nd line
+                fileContent += `${ item.track.artists[0].name } - ${ item.track.name }.mp3\n`;
             }
-        };
-        XHR.send();
+
+            progress.value++;
+        }
+
+        clog(tracksOffset);
+        if (tracksOffset >= tracksTotal) {
+
+            // create file
+            let file = new Blob([fileContent], {type: type});
+
+            // prepare download
+            clog("Preparing download...", 1);
+            downloadBtn.disabled = false;
+            downloadA.download = `${ playlistName }.${ type }`;
+            downloadA.href = URL.createObjectURL(file);
+            downloadBtn.innerHTML = `Download <code>${ playlistName }.${ type.toUpperCase() }</code>`
+            
+            // initiate download
+            downloadA.click();
+            clog(`Downloaded file \`${ playlistName }.${ type }\``, 2);
+        } else {
+            // call `fetchTracksRecursive(tracksOffset, tracksTotal)` again for the next batch
+            await fetchTracksRecursive(tracksOffset + stepLimit, tracksTotal);
+        }
     }
 
     // call `fetchTracksRecursive(tracksOffset, tracksTotal)` for the first time
-    fetchTracksRecursive(0, tracksTotal)
+    await fetchTracksRecursive(0, tracksTotal);
 }
+
+await fetchPlaylists();
